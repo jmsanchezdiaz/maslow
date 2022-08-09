4bdebaf371b34749eb5e710bf8b638c48a302f71
Juan Manuel Sanchez Diaz <jmsanchezdiaz02@gmail.com>
2022-08-08 22:23:48 -0300

    initial work

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Card = ({ title, subtitle, multiplier, value, minValue, maxValue }) => {
+  return (
+    <section className="shadow-lg p-4 rounded-md flex flex-col ">
+      <div className="flex justify-between align-middle">
+        <div>
+          <h2 className="font-semibold text-lg">{title.toUpperCase()}</h2>
+          <h4 className="opacity-60 text-xs">Mensual</h4>
+          <h4 className="opacity-60 text-xs">ARS</h4>
+          {subtitle && <h4 className="opacity-60 text-xs">{subtitle}</h4>}
+        </div>
+        <span className="font-semibold">{multiplier}x</span>
+      </div>
+
+      <button
+        className="w-5 h-5 self-end rounded-md"
+        aria-label="edit slider button"
+      >
+        <img alt="pencil" src="/icons/pencil.svg" />
+      </button>
+    </section>
+  )
+}
+
+export default Card
